0e2657580416ec977c4bdaadfb15cfe8b248165c
Tianhe Wu <tianhe_wu@foxmail.com>
2021-06-20 02:30:33 +0800

    finish maze visualization

---
+++ final_car.ino
@@ -0,0 +1,543 @@
+#include "MsTimer2.h"
+
+#define MAX_NUM_POINT 50
+#define MAX_NUM_VECTOR 50
+#define MAX_NUM_QUEUE 200
+#define N 5
+#define M 5
+
+#define DEFAULT_FB_POWER 0.2
+#define DEFAULT_LR_POWER 0.18
+#define TIMEOUT 500;
+
+int traverse_end = 0;
+int cur_dir = 1;
+int cur_i = 4, cur_j = 4;
+int auto_drive = 0;
+
+template <typename T>
+class queue {
+private:
+  int m_size;
+  int q_front;
+  int rear;
+  T* arr;
+  
+public:
+  queue();
+  void push(T item);
+  void pop();
+  T front();
+  bool empty();
+};
+
+template <typename T>
+queue<T>::queue() {
+  m_size = MAX_NUM_QUEUE;
+  arr = new T[m_size];
+  q_front = 0;
+  rear = 0;
+}
+
+template <typename T>
+void queue<T>::push(T item) {
+  arr[rear] = item;
+  rear = (rear + 1) % m_size;
+}
+
+template <typename T>
+void queue<T>::pop() {
+  q_front = (q_front + 1) % m_size;
+}
+
+template <typename T>
+T queue<T>::front() {
+  return arr[q_front];
+}
+
+template <typename T>
+bool queue<T>::empty() {
+  return q_front == rear;
+}
+
+
+// define vector
+template <typename T>
+class vector {
+private:
+  int my_first;
+  int my_last;
+  T arr[MAX_NUM_VECTOR];
+  
+public:
+  vector();
+  void push_back(T item);
+  int size();
+  T& operator[](int i) {
+    return arr[i];
+  }
+};
+
+template <typename T>
+vector<T>::vector() {
+  my_first = 0;
+  my_last = -1;
+}
+
+template <typename T>
+void vector<T>::push_back(T item) {
+  my_last++;
+  arr[my_last] = item;
+}
+
+template <typename T>
+int vector<T>::size() {
+  return my_last - my_first + 1;
+}
+
+struct Point {
+  int x, y;
+};
+
+struct Edge {
+  int u, v;
+  char c;
+  Edge() {}
+  Edge(int _u, int _v, char _c) : u(_u), v(_v), c(_c) {}
+};
+
+
+Point s_pos, e_pos;
+int s_idx, e_idx;
+int d[MAX_NUM_POINT];
+
+// need to change init_start_end()
+char _map[N][M] = {{'.', 'X', '.', '.', '.'},
+                   {'.', '.', 'E', 'X', '.'},
+                   {'.', 'X', 'X', 'X', '.'}, 
+                   {'.', 'X', 'X', 'X', '.'}, 
+                   {'.', '.', '.', '.', 'S'}};
+
+bool visited[MAX_NUM_POINT];
+vector<char> ans;
+vector<Edge> edges;
+vector<int> G[MAX_NUM_POINT];
+
+
+void init_start_end() {
+  s_pos.x = 4;
+  s_pos.y = 4;
+  e_pos.x = 1;
+  e_pos.y = 2;
+  s_idx = s_pos.x * M + s_pos.y;
+  e_idx = e_pos.x * M + e_pos.y;
+}
+
+
+void add_edge(int u, int v, char c) {
+  edges.push_back(Edge(u, v, c));
+  int index = edges.size() - 1;
+  G[u].push_back(index);
+}
+
+
+void init_map() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      int cur_idx = i * M + j;
+      if (_map[i][j] == 'X')
+        continue;
+      if (i - 1 >= 0 && (_map[i - 1][j] == '.' || _map[i - 1][j] == 'E' || _map[i - 1][j] == 'S'))
+        add_edge(cur_idx, (i - 1) * M + j, 'U');
+      if (i + 1 < N && (_map[i + 1][j] == '.' || _map[i + 1][j] == 'E' || _map[i + 1][j] == 'S'))
+        add_edge(cur_idx, (i + 1) * M + j, 'D');
+      if (j - 1 >= 0 && (_map[i][j - 1] == '.' || _map[i][j - 1] == 'E' || _map[i][j - 1] == 'S'))
+        add_edge(cur_idx, i * M + j - 1, 'L');
+      if (j + 1 < M && (_map[i][j + 1] == '.' || _map[i][j + 1] == 'E' || _map[i][j + 1] == 'S'))
+        add_edge(cur_idx, i * M + j + 1, 'R');
+    }
+  }
+}
+
+
+void rev_bfs() {
+  for (int i = 0; i < MAX_NUM_POINT; i++)
+    visited[i] = false;
+  queue<int> que;
+  que.push(e_idx);
+  visited[e_idx] = true;
+  d[e_idx] = 0;
+  while (!que.empty()) {
+    int u = que.front();
+    que.pop();
+    for (int i = 0; i < G[u].size(); i++) {
+      int e = G[u][i];
+      int v = edges[e].v;
+      if (!visited[v]) {
+        visited[v] = true;
+        d[v] = d[u] + 1;
+        que.push(v);
+      }
+    }
+  }
+}
+
+void bfs() {
+  queue<int> que;
+  que.push(s_idx);
+  while (!que.empty()) {
+    int ele = que.front();
+    que.pop();
+    int node = -1;
+    char min_c = 'z';
+    for (int i = 0; i < G[ele].size(); i++) {
+      int u = G[ele][i];
+      if (d[ele] - d[edges[u].v] == 1 && edges[u].c < min_c) {
+        node = edges[u].v;
+        min_c = min(min_c, edges[u].c);
+      }
+    }
+    if (node != -1) {
+      que.push(node);
+      ans.push_back(min_c);
+    }
+  }
+}
+
+
+// pinmode define
+#define MOTOR_C_P 10
+#define MOTOR_C_N 11
+#define MOTOR_D_P 12
+#define MOTOR_D_N 13
+#define MOTOR_A_P 6
+#define MOTOR_A_N 7
+#define MOTOR_B_P 5
+#define MOTOR_B_N 4
+
+#define LED_PIN 15
+#define IS_OBSTACLE_PIN 2
+
+void stop_car() {
+  analogWrite(MOTOR_C_P, 0);
+  analogWrite(MOTOR_C_N, 0);
+  analogWrite(MOTOR_B_N, 0);
+  analogWrite(MOTOR_B_P, 0);
+  analogWrite(MOTOR_A_P, 0);
+  analogWrite(MOTOR_A_N, 0);
+  analogWrite(MOTOR_D_P, 0);
+  analogWrite(MOTOR_D_N, 0);
+}
+
+
+void turn_right(float power) {
+  motor_A_forward(power);
+  motor_B_backward(power);
+  motor_C_backward(power);
+  motor_D_forward(power);
+}
+
+
+void turn_left(float power) {
+  motor_A_backward(power);
+  motor_B_forward(power);
+  motor_C_forward(power);
+  motor_D_backward(power);
+}
+
+
+void move_forward(float power) {
+  motor_A_forward(power);
+  motor_B_forward(power);
+  motor_C_forward(power);
+  motor_D_forward(power);
+}
+
+
+void move_backward(float power) {
+  motor_A_backward(power);
+  motor_B_backward(power);
+  motor_C_backward(power);
+  motor_D_backward(power);
+}
+
+
+void move_right(float power) {
+  motor_A_forward(power);
+  motor_B_backward(power);
+  motor_C_forward(power);
+  motor_D_backward(power);
+}
+
+
+void move_left(float power) {
+  motor_A_backward(power);
+  motor_B_forward(power);
+  motor_C_backward(power);
+  motor_D_forward(power);
+}
+
+
+int get_power(float power) {
+  return int(255 * power);
+}
+
+
+void motor_A_forward(float power) {
+  analogWrite(MOTOR_A_P, get_power(power));
+  analogWrite(MOTOR_A_N, 0);
+}
+
+
+void motor_B_forward(float power) {
+  analogWrite(MOTOR_B_N, 0);
+  analogWrite(MOTOR_B_P, get_power(power));
+}
+
+
+void motor_C_forward(float power) {
+  analogWrite(MOTOR_C_P, 0);
+  analogWrite(MOTOR_C_N, get_power(power));
+}
+
+
+void motor_D_forward(float power) {
+  analogWrite(MOTOR_D_N, 0);
+  analogWrite(MOTOR_D_P, get_power(power));
+}
+
+
+void motor_A_backward(float power) {
+  analogWrite(MOTOR_A_P, 0);
+  analogWrite(MOTOR_A_N, get_power(power));
+}
+
+
+void motor_B_backward(float power) {
+  analogWrite(MOTOR_B_N, get_power(power));
+  analogWrite(MOTOR_B_P, 0);
+}
+
+
+void motor_C_backward(float power) {
+  analogWrite(MOTOR_C_P, get_power(power));
+  analogWrite(MOTOR_C_N, 0);
+}
+
+
+void motor_D_backward(float power) {
+  analogWrite(MOTOR_D_N, get_power(power));
+  analogWrite(MOTOR_D_P, 0);
+}
+
+#define STOP_DISTANCE 400 
+unsigned int high_len_3 = 0;
+unsigned int low_len_3 = 0;
+unsigned int len_mm_3 = 0;
+
+int cal_distance_s3() {
+  Serial3.flush();
+  Serial3.write(0X55);
+  delay(500);
+  if (Serial3.available() >= 2) {
+      high_len_3 = Serial3.read();
+      low_len_3 = Serial3.read();
+      len_mm_3 = high_len_3 * 256 + low_len_3;
+  }
+  return len_mm_3;
+}
+
+int is_obstacle = HIGH;
+enum {
+  FRONT = 1,
+  LEFT,
+  BACK,
+  RIGHT
+};
+
+void fill_map(int i, int j) {
+  is_obstacle = digitalRead(IS_OBSTACLE_PIN);
+  if (is_obstacle == LOW) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+      int one_op = 0;
+      digitalWrite(LED_PIN, LOW);
+      switch(cur_dir) {
+        case FRONT: 
+           if (i - 1 >= 0) {
+             _map[i - 1][j] = '.';
+             if (cur_i > 0)
+               cur_i--;
+           }
+           break;
+        case LEFT:
+           if (j - 1 >= 0) {
+             _map[i][j - 1] = '.';
+             if (cur_j > 0)
+               cur_j--;
+           }
+           break;
+        case BACK: 
+           if (i + 1 < N) {
+             _map[i + 1][j] = '.';
+             if (cur_i < N - 1)
+               cur_i++;
+           }
+           break;
+        case RIGHT: 
+           if (j + 1 < M) {
+             _map[i][j + 1] = '.';
+             if (cur_j < M - 1)
+               cur_j++;
+           }
+           break;
+      }
+  }
+}
+
+void setup() {
+  pinMode(MOTOR_A_P, OUTPUT);
+  pinMode(MOTOR_A_N, OUTPUT);
+  pinMode(MOTOR_B_P, OUTPUT);
+  pinMode(MOTOR_B_N, OUTPUT);
+  pinMode(MOTOR_C_P, OUTPUT);
+  pinMode(MOTOR_C_N, OUTPUT);
+  pinMode(MOTOR_D_P, OUTPUT);
+  pinMode(MOTOR_D_N, OUTPUT);
+
+  Serial3.begin(9600);
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(IS_OBSTACLE_PIN, INPUT);
+}
+
+enum {
+  STOP,
+  FORWARD,
+  BACKWARD,
+  TRUN_LEFT,
+  TRUN_RIGHT
+};
+
+
+int _state = STOP;
+int _timeleft = 0;
+int tick = 0;
+
+void loop() {
+  // fill_map(cur_i, cur_j);
+  while (Serial.available() > 1 && (!auto_drive)) {
+    byte cmd = Serial.read(), key;
+    switch (cmd) {
+      case 'D':
+        key = Serial.read();
+        int next_state;
+        switch (key) {
+          case 70: cur_dir = FRONT; continue;
+          case 76: cur_dir = LEFT; continue;
+          case 82: cur_dir = RIGHT; continue;
+          case 66: cur_dir = BACK; continue;
+          case 69: traverse_end = 1; continue;
+          case 87: next_state = FORWARD; break;
+          case 83: next_state = BACKWARD; break;
+          case 65: next_state = TRUN_LEFT; break;
+          case 68: next_state = TRUN_RIGHT; break;
+          case 80: auto_drive = 1; continue;
+          default: continue;
+        }
+        if (_state == next_state) {
+          _timeleft = TIMEOUT;
+        } else {
+          stop_car();
+          _state = next_state;
+          switch (_state) {
+            case FORWARD: move_forward(DEFAULT_FB_POWER); break;
+            case BACKWARD: move_backward(DEFAULT_FB_POWER); break;
+            case TRUN_LEFT: turn_left(DEFAULT_LR_POWER); break;
+            case TRUN_RIGHT: turn_right(DEFAULT_LR_POWER); break;
+          }
+          _timeleft = TIMEOUT;
+        }
+        break;
+      case 'U':
+        key = Serial.read();
+        int cancel_state;
+        switch (key) {
+          case 87: cancel_state = FORWARD; break;
+          case 83: cancel_state = BACKWARD; break;
+          case 65: cancel_state = TRUN_LEFT; break;
+          case 68: cancel_state = TRUN_RIGHT; break;
+          default: continue;
+        }
+        if (_state == cancel_state) {
+          _state = STOP;
+          stop_car();
+        }
+        break;
+      case 'F':
+        key = Serial.read();
+        break;
+    }
+  }
+  if (_state != STOP) {
+    if (!--_timeleft) {
+      _state = STOP;
+      stop_car();
+    }
+  }
+  delay(1);
+  while (traverse_end) {
+    stop_car();
+    init_start_end();
+    init_map();
+    rev_bfs();
+    bfs();
+    if (d[s_idx] == 0)
+      Serial.println("No path!");
+    else {
+      Serial.println("Shortest Path:");
+      char print_ans[ans.size()];
+      for (int i = 0; i < ans.size(); i++)
+        print_ans[i] = ans[i];
+      Serial.println(print_ans);
+    }
+    Serial.println("MAP");
+    traverse_end = 0;
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++)
+        Serial.println(_map[i][j]);
+    }
+  }
+  while (auto_drive) {
+    enum {
+      S, U, L, R, D
+    };
+    int max_ticks = 2000;
+    int cur_state = S;
+    for (int i = 0; i < ans.size(); i++) {
+      int next_state;
+      if (cal_distance_s3() < 200) {
+        next_state = 0;
+      }
+      if (ans[i] == 'U') {
+        next_state = 1;
+      }
+      else if (ans[i] == 'L') {
+        next_state = 2;
+      }
+      else if (ans[i] == 'D') {
+        next_state = 4;
+      }
+      while (cal_distance_s3() > 200)
+        move_forward(0.09);
+      if ((cur_state == 1 && next_state == 2) || (cur_state == 2 && next_state == 4)) {
+        stop_car();
+        while (cal_distance_s3() < 600)
+          turn_left(DEFAULT_LR_POWER);
+      }
+    }
+    auto_drive = 0;
+    stop_car();
+    break;
+  }
+}
---
+++ pc.py
@@ -0,0 +1,134 @@
+import serial
+import tkinter as tk
+
+
+init_x = 75
+init_y = 100
+path_exist = True
+shortest_path = ''
+
+ser = serial.Serial('COM9')
+print("open serial", ser.name)         # check which port was really used
+
+root_move = tk.Tk()
+root_move.title("car move")
+root_map = tk.Tk()
+root_map.title("map")
+
+
+def get_text():
+    print('get text...')
+    main_text = ''
+    while True:
+        data = ser.readline()
+        data = data.strip()
+        data = data.decode('utf-8', 'ignore')
+        print('data:', data)
+        if data == 'Shortest Path:':
+            path = ser.readline()
+            path = path.strip()
+            path = path.decode('utf-8', 'ignore')
+            global shortest_path
+            shortest_path = path
+            print('data:', path)
+        if data == "MAP":
+            count = 0
+            print('get map text...')
+            while count < 25:
+                point_text = ser.readline()
+                point_text = point_text.strip()
+                point_text = point_text.decode('utf-8', 'ignore')
+                main_text += point_text
+                count += 1
+            break
+        if data == 'No path!':
+            path_exist = False
+    print(main_text)
+    return main_text
+
+
+def transform_str2map():
+    print('build map...')
+    text_map = get_text()
+    if path_exist == False:
+        for i in range(len(text_map)):
+            if text_map[i] == '.':
+                text_map[i] = 'X'
+    _map = [[0 for i in range(5)] for j in range(5)]
+    _i = 0
+    _j = 0
+    count = 0
+    for i in range(len(text_map)):
+        if text_map[i] == 'X':
+            _map[_i][_j] = 'X'
+        elif text_map[i] == '.':
+            _map[_i][_j] = '.'
+        elif text_map[i] == 'E':
+            _map[_i][_j] = 'E'
+        elif text_map[i] == 'S':
+            _map[_i][_j] = 'S'
+        count += 1
+        _j += 1
+        if count % 5 == 0:
+            _i += 1
+            count = 0
+            _j = 0
+    print('map:')
+    print(_map)
+    return _map
+
+
+def draw_rectangle(canvas, x, y, color):
+    canvas.create_rectangle(x, y, x + 70, y + 70, fill=color, outline='black')
+
+
+def generate_map(_root, _map):
+    canvas = tk.Canvas(_root, width=500, height=500, bg="white")
+    canvas.create_text(250, 35, text='THE MAZE MAP ARDUINO SEND')
+    canvas.create_text(350, 55, text='Shortest path: ' + shortest_path)
+    canvas.create_text(350, 75, text='Created by Wu Tianhe')
+    cur_x = init_x
+    cur_y = init_y
+    for i in range(len(_map)):
+        for j in range(5):
+            if _map[i][j] == 'X':
+                draw_rectangle(canvas, cur_x, cur_y, 'black')
+            elif _map[i][j] == '.':
+                draw_rectangle(canvas, cur_x, cur_y, 'white')
+            elif _map[i][j] == 'E':
+                draw_rectangle(canvas, cur_x, cur_y, 'green')
+                canvas.create_text(cur_x + 35, cur_y + 35, text='E')
+            elif _map[i][j] == 'S':
+                draw_rectangle(canvas, cur_x, cur_y, 'yellow')
+                canvas.create_text(cur_x + 35, cur_y + 35, text='S')
+            cur_x += 70
+        cur_y += 70
+        cur_x = init_x
+    canvas.pack()
+
+
+def key_up(e):
+    ser.write(('U' + e.char.upper()).encode('ASCII'))
+    print('up', e.char)
+
+
+def key_down(e):
+    ser.write(('D' + e.char.upper()).encode('ASCII'))
+    print('down', e.char)
+    if e.char == 'e':
+        _map = transform_str2map()
+        generate_map(root_map, _map)
+        root_map.mainloop()
+
+
+frame = tk.Frame(root_move, width=200, height=200)
+frame.bind("<KeyPress>", key_down)
+frame.bind("<KeyRelease>", key_up)
+frame.focus_set()
+# display
+frame.pack()
+root_move.mainloop()
+
+
+
+
